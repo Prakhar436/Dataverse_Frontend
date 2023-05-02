f694a5e92b9a79969b73cd1d4f8db60741a11271
Prakhar Pandey <personal.prakhar@gmail.com>
2023-05-02 22:25:19 +0530

    Improved Navbar 2/5/23

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,15 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='header'>
-      <div className="container">
-      <div className="nav">
-     <img width={150} src={DataVerseLogo} alt="" />
-     </div>
-     <div >
-      <ul className='links'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li> 
+    <navbar className='header'>
+     <ul className='links'>
+       <li>Home</li>
+       <li>About Us</li>
+       <li>Events Gallery</li>
+       <li>Roadmaps</li>
+       <li>Community</li>
       </ul>
-     </div>
-     </div>
-    </div>
+    </navbar>
   )
 }
 
